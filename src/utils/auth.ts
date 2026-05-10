2126be4216e675c443ec46b9f5968dc21e79bd06
Tan Ga Foo <chevaliertann@gmail.com>
2026-05-10 15:14:25 +0800

    Add better auth schema - pushed first db (yay!) - configure auth.ts to be working

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,13 @@
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-export const auth = betterAuth({});
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+});
